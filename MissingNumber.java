8795b931e87eb1dc722fe9ff2d5d5deef5fdb7aa
Shubashini S <shubashini211@gmail.com>
2026-09-17 13:53:43 +0530

    Add Missing Number

---
+++ MissingNumber.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(), sum=0;
+        for(int i=0;i<n-1;i++) sum+=sc.nextInt();
+        System.out.println(n*(n+1)/2-sum);
+    }
+}
