2dd7b0ba9e17c789d6f979456c5b50cac226c1f3
Shubashini S <shubashini211@gmail.com>
2026-09-17 13:54:03 +0530

    Add Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in); int n=sc.nextInt(); int[] a=new int[n];
+        for(int i=0;i<n;i++) a[i]=sc.nextInt(); int t=sc.nextInt();
+        for(int i=0;i<n;i++) for(int j=i+1;j<n;j++) if(a[i]+a[j]==t){System.out.println(i+" "+j); return;}
+        System.out.println("-1");
+    }
+}
