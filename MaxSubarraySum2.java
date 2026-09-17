ff416a1eb48ac4f2ed98ae597a37988b50854d96
Shubashini S <shubashini211@gmail.com>
2026-09-17 13:59:20 +0530

    Add maximum subarray sum solution

---
+++ MaxSubarraySum2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in); int n=sc.nextInt(),sum=0,max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){int x=sc.nextInt();sum=Math.max(x,sum+x);max=Math.max(max,sum);}
+        System.out.println(max);
+    }
+}
